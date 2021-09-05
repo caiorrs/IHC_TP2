c132443684c04bb5552fa9d4401f92f98db0f999
Caio Roberto <caiorrs@gmail.com>
2021-09-05 11:34:44 -0300

    Atividade 2

--- App.js
+++ App.js
@@ -1,111 +1,116 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Home = () => {
+  const navigation = useNavigation();
+
+  const [text, setText] = useState('');
+
+  const onSendMessage = () => {
+    navigation.navigate('Details', {text});
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.homeScreen}>
+      <View style={styles.homeContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Enter a message"
+            value={text}
+            onChangeText={setText}
+          />
+        </View>
+        <TouchableOpacity style={styles.buttonSend} onPress={onSendMessage}>
+          <Text style={styles.buttonText}>SEND</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Details = ({route}) => {
+  const text = route?.params?.text || 'No text was written';
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.details}>
+      <Text style={styles.messageTitle}>Mensagem enviada da outra tela:</Text>
+      <Text style={styles.message}>{text}</Text>
+    </View>
+  );
+};
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{title: 'Atividade 2'}}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{title: 'Atividade 2'}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  inputContainer: {
+    borderRadius: 10,
+    flex: 0.7,
+    borderBottomWidth: 2,
+    borderBottomColor: '#888',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  homeScreen: {
+    flex: 1,
+    padding: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+  homeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttonSend: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#3700B3',
+    flex: 0.2,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  details: {
+    padding: 20,
+  },
+  messageTitle: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  message: {
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
