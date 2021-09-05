e969915473012bb3575f6de7e02f5c0572393e54
Caio Roberto <caiorrs@gmail.com>
2021-09-05 13:34:36 -0300

    Parte 2

--- App.js
+++ App.js
@@ -1,111 +1,202 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  DeviceEventEmitter,
+  NativeModules,
+  Platform,
+  PermissionsAndroid,
+  ToastAndroid,
+  Alert,
+  Linking,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {gyroscope, setUpdateIntervalForType} from 'react-native-sensors';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+import Geolocation from 'react-native-geolocation-service';
+
+setUpdateIntervalForType('gyroscope', 500);
+
+const App = () => {
+  const {SensorManager} = NativeModules;
+
+  SensorManager.startLightSensor(500);
+
+  const [light, setLight] = useState(0);
+  const [gyro, setGyro] = useState({x: 0, y: 0, z: 0});
+  const [bgColor, setBgColor] = useState('#FFF');
+  const [textColor, setTextColor] = useState('#000');
+  const [location, setLocation] = useState(null);
+
+  const hasPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow "${appConfig.displayName}" to determine your location.`,
+        '',
+        [
+          {text: 'Go to Settings', onPress: openSetting},
+          {text: "Don't Use Location", onPress: () => {}},
+        ],
+      );
+    }
+
+    return false;
+  };
+
+  const hasLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      const hasPermission = await hasPermissionIOS();
+      return hasPermission;
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+      return true;
+    }
+
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
+
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      ToastAndroid.show(
+        'Location permission denied by user.',
+        ToastAndroid.LONG,
+      );
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
+    }
+
+    return false;
+  };
+
+  const getCurrentLocation = async () => {
+    const hasPermission = await hasLocationPermission();
+    if (!hasPermission) {
+      return;
+    }
+
+    Geolocation.getCurrentPosition(
+      position => {
+        setLocation(position);
+      },
+      error => {
+        Alert.alert(`Code ${error.code}`, error.message);
+        setLocation(null);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+        forceRequestLocation: true,
+        forceLocationManager: true,
+        showLocationDialog: true,
+      },
+    );
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+    gyroscope.subscribe(({x, y, z}) => {
+      setGyro({x, y, z});
+    });
+    DeviceEventEmitter.addListener('LightSensor', data => {
+      setLight(data.light);
+    });
+
+    return () => DeviceEventEmitter.removeAllListeners();
+  }, []);
+
+  useEffect(() => {
+    if (light < 200) {
+      setBgColor('#FFF');
+      setTextColor('#000');
+    } else if (light < 800) {
+      setBgColor('#777');
+      setTextColor('#FFF');
+    } else if (light < 1000) {
+      setBgColor('#333');
+      setTextColor('#FFF');
+    } else if (light > 1000) {
+      setBgColor('#000');
+      setTextColor('#FFF');
+    }
+  }, [light]);
+
   return (
-    <View style={styles.sectionContainer}>
+    <View
+      style={{
+        ...styles.container,
+        backgroundColor: bgColor,
+      }}>
       <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
+        style={{
+          ...styles.text,
+          color: textColor,
+        }}>{`Light data: ${light.toFixed(2)}`}</Text>
       <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+        style={{
+          ...styles.text,
+          color: textColor,
+        }}>{`Gyro data: X: ${gyro.x.toFixed(2)} Y: ${gyro.y.toFixed(
+        2,
+      )} Z: ${gyro.z.toFixed(2)}`}</Text>
+      <Text
+        style={{
+          ...styles.text,
+          color: textColor,
+        }}>{`Geolocation: Lat: ${
+        location?.coords?.latitude.toFixed(3) || -1
+      } Lon: ${location?.coords?.longitude.toFixed(3) || -1}`}</Text>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
